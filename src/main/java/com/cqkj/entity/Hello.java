b7472a3b7c4845086ee0157b1e8b6f95558d6bac
HlbisweitG <879277889@qq.com>
2019-01-18 13:24:07 +0800

    first commit

---
+++ src/main/java/com/cqkj/entity/Hello.java
@@ -0,0 +1,14 @@
+package com.cqkj.entity;
+
+public class Hello
+{
+    public void say()
+    {
+    	System.out.println("hello world !");
+    }
+    
+    public String hellodemo()
+    {
+    	return "HELLO WORLD!";
+    }
+}
